8cdc9cf0ac28566c667a5f3b434a797f80879d14
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2025-01-12 21:30:44 +0530

    feat: updated hero section

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import type { AnimatedProps } from "@/types/motion";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import LitopiaLogo from "./../../assets/logo.webp";
+import MedicapsLogo from "./../../assets/medicaps_logo.png";
+import YearsLogo from "./../../assets/logo_25.png";
+import SahityikLogo from "./../../assets/sahityik_logo.png";
 
 export function Hero({ id }: AnimatedProps) {
   return (
     <section
       id={id}
-      className="relative min-h-screen flex items-center justify-center px-4 overflow-hidden"
+      className="relative min-h-screen flex items-center justify-center px-4 overflow-hidden hero-section"
     >
       <div className="absolute inset-0 pointer-events-none">
         <div className="absolute -top-1/4 -left-1/4 w-[1000px] h-[1000px] bg-gradient-to-r from-pink-500/30 via-purple-500/30 to-red-500/30 dark:from-pink-500/80 dark:via-purple-500/20 dark:to-red-500/20 rounded-full blur-3xl animate-slow-spin"></div>
@@ -19,35 +23,36 @@ export function Hero({ id }: AnimatedProps) {
         <div className="absolute inset-0 opacity-30 mix-blend-overlay"></div>
       </div>
       <div className="relative max-w-4xl mx-auto text-center">
-        <div className="relative">
-          <motion.h1
-            className="text-6xl md:text-7xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-foreground via-foreground/90 to-foreground/80 pb-2"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, ease: "easeOut" }}
-          >
-            lorem ipsum
-            <br />
-            dolor sit amet
-          </motion.h1>
-          <motion.div
-            className="absolute -bottom-4 left-1/2 -translate-x-1/2 w-32 h-1.5 bg-gradient-to-r from-transparent via-foreground/30 to-transparent rounded-full"
-            initial={{ scaleX: 0 }}
-            animate={{ scaleX: 1 }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
-          ></motion.div>
-        </div>
         <motion.p
-          className="text-lg md:text-xl text-foreground/70 mb-8 max-w-2xl mx-auto mt-12"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          asperiores fuga ut quidem rerum fugit optio illum dicta esse veritatis
-          vel inventore officia, assumenda nostrum nisi voluptatem, laudantium
-          tenetur est!
+          <Image
+            className="block mx-auto drop-shadow-2xl contrast-125"
+            width={500}
+            // height={500}
+            alt="Litopia Logo"
+            src={LitopiaLogo}
+          />
         </motion.p>
+
+        <motion.h1
+          className="text-6xl text-[#222111] md:text-8xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-foreground via-foreground/90 to-foreground/80 pb-2"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          The Literature Fest
+        </motion.h1>
+        <motion.h6
+          className="text-[#222111] text-xl uppercase font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-foreground via-foreground/90 to-foreground/80 "
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          Presented By
+        </motion.h6>
 
         <div className="flex items-center justify-center gap-4 relative">
           <motion.div
@@ -57,9 +62,13 @@ export function Hero({ id }: AnimatedProps) {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            <Button className="bg-gradient-to-r from-foreground via-foreground to-foreground/90 text-background hover:opacity-90 px-8 py-6 text-lg rounded-full transition-all duration-300">
-              Get Started
-            </Button>
+            <Image
+              className="block mx-auto drop-shadow-2xl contrast-125"
+              width={100}
+              // height={500}
+              alt="Medicaps Logo"
+              src={MedicapsLogo}
+            />
           </motion.div>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
@@ -68,18 +77,28 @@ export function Hero({ id }: AnimatedProps) {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            <Button
-              variant="outline"
-              className="border-foreground/20 text-foreground hover:bg-foreground/10 px-8 py-6 text-lg rounded-full transition-all duration-300"
-            >
-              <a
-                href="https://github.com/anibalalpizar/nextjs-shadcn-landing"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Code Source
-              </a>
-            </Button>
+            <Image
+              className="block mx-auto drop-shadow-2xl contrast-125"
+              width={100}
+              // height={500}
+              alt="25 years Logo"
+              src={YearsLogo}
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <Image
+              className="block mx-auto drop-shadow-2xl contrast-125"
+              width={100}
+              // height={500}
+              alt="Sahityik Logo"
+              src={SahityikLogo}
+            />
           </motion.div>
         </div>
       </div>
