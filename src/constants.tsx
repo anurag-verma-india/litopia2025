f41dd02ea6deba81deb54913798b1ab5010b7f17
anurag-verma-india <104461155+anurag-verma-india@users.noreply.github.com>
2025-02-20 15:43:39 +0530

    Added Devendra's photo

--- src/constants.tsx
+++ src/constants.tsx
@@ -32,7 +32,8 @@ import tasneem from "@/assets/team/tasneem.webp";
 import mannat from "@/assets/team/mannat.webp";
 import riddhima from "@/assets/team/ridhima.webp";
 import abbas from "@/assets/team/abbas.webp";
-import devendra from "@/assets/team/600x400.svg";
+// import devendra from "@/assets/team/600x400.svg";
+import devendra from "@/assets/team/devendra.webp"
 
 import DrRupaliChaturvedi from "@/assets/faculty/Dr. Rupali Chaturvedi.png";
 import DrShaliniModh from "@/assets/faculty/Dr. Shalini Modh.jpg";
